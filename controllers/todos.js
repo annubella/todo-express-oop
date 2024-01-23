b6e58638e4e0f0e2202597a297e651fda1e183f7
Annabella <eliisaannaloiv@gmail.com>
2024-01-23 17:51:28 +0200

    Get all Todos

--- controllers/todos.js
+++ controllers/todos.js
@@ -19,6 +19,9 @@ class todoController{
             newTask: newTodo
         })     
     } 
+    getTodos(req, res){
+        res.json({tasks: this.TODOS} )
+    } 
 }
 
 export const TodoController = new todoController()
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use(express.urlencoded({extended: true}))
+
 app.use('/todos', todoRoutes)
 
 app.listen(3009,()=> { 
